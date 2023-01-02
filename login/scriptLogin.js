1237d68073f4cb202f0edfda82479f39f3528d65
noamsin2 <noamsin2@gmail.com>
2023-01-02 14:55:22 +0200

    checkpassword fix

--- login/scriptLogin.js
+++ login/scriptLogin.js
@@ -3,16 +3,16 @@ let arr_students =
 let arr_hr = JSON.parse(localStorage.getItem("HR")) || [];
 let arr_inspector = JSON.parse(localStorage.getItem("Inspectors")) || [];
 
-// function checkPassword(password){
-//   //Password must be 8 to 15 characters which contain at least one lowercase letter,
-//   //one uppercase letter, one numeric digit, and one special character
-//   var passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+function checkPassword(password){
+  //Password must be 8 to 15 characters which contain at least one lowercase letter,
+  //one uppercase letter, one numeric digit, and one special character
+  var passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
   
-//   if(password.match(passw)){
-//     return true;
-//   }
-//   return false;
-// };
+  if(password.match(passw)){
+    return true;
+  }
+  return false;
+};
 
 function signinaction(e) {
   event.preventDefault();
@@ -51,7 +51,7 @@ function signinaction(e) {
 };
 // };
 
-function changepassword(event) {
+function changepassword(e) {
   event.preventDefault();
   const email = document.getElementById('emailUser').value;
   const question = document.getElementById('safetyQuestion').value;
@@ -63,18 +63,16 @@ function changepassword(event) {
   else if (newpassword != confirmpassword) {
     document.getElementById('texto').innerHTML ="Passwords mismatch";
   }
-  // else if(checkPassword(newpassword)){
-  //   document.getElementById('texto').innerHTML ="The password is too weak!";
-  // }
+  else if(!checkPassword(newpassword)){
+    document.getElementById('texto').innerHTML ="The password is too weak!";
+  }
   else{
-  let arr_students =
-      JSON.parse(localStorage.getItem("Students and Graduates")) || []; 
-      for (let key in arr_students) {
+    for (let key in arr_students) {
       if (arr_students[key].username == email && arr_students[key].question == question){
         arr_students[key].password=newpassword;
         arr_students[key].confirm_password=confirmpassword;
         localStorage.setItem( "Students and Graduates",JSON.stringify(arr_students));  
-                
+        window.location.href = "../login/login_n.html";
       }
     }
   }
