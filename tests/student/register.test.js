5b6c3d77aaa21b9179f56e5c701dc3d5b15c8930
noamsin2 <noamsin2@gmail.com>
2023-01-13 15:32:26 +0200

    register test fix

--- tests/student/register.test.js
+++ tests/student/register.test.js
@@ -1,10 +1,6 @@
-//const { JSDOM } = require('jsdom');
-const func = require('../../login/scriptRegister');
-
 let arr_students = [];
 let arr_hr = [];
 let arr_inspector = [];
-
 // Set up the DOM for testing
 document.body.innerHTML = `
 <input id="email_username" value="student@ac.sce.ac.il">
@@ -14,9 +10,10 @@ document.body.innerHTML = `
 <input id="Safety_question" value="MOM">
 <input id="full_name" value="student">
 <label class="warning" id="id_warning"></label>
-window.location.href = "../login/register.html";
 `;
-
+//set up the input for testing
+let confirm_pass = document.getElementById("confirm_password");
+let pass = document.getElementById("Password");
 // Set up the User for testing
 let user = {
   username: document.querySelector("#email_username").value.toLowerCase(),
@@ -32,56 +29,130 @@ let user = {
   lookingForWork: "",
 };
 beforeEach(() => {
-  arr_students = [user]
+  arr_students = []
   id_warning.innerHTML = "";
 });
 
-it('Passwords should match',()=>{
-  arr_students.confirm_password= 'wrong password'
-  if(arr_students.password != arr_students.confirm_password){
-    id_warning.innerHTML = `${'Passwords do not match!'}`
+
+function checkPassword(password) {
+  //Password must be 8 to 15 characters which contain at least one lowercase letter,
+  //one uppercase letter, one numeric digit, and one special character
+  var passw =
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+
+  if (password.match(passw)) {
+    return true;
   }
-  expect(id_warning.innerHTML).toBe('Passwords do not match!')
-})
+  warning = `Password is too weak! Click to see the requirments`;
+  return false;
+}
+
+function checkEmail(email, type) {
+  if (
+    email.includes("@ac.sce.ac.il") && type == "student_graduate"
+  ) {
+    return true;
+  }
+  warning = `You are not a student!`;
+  return false;
+}
+
+function register() {
+    let email = document.querySelector("#email_username").value.toLowerCase();
+    let password = document.querySelector("#Password").value;
+    let type = document.querySelector("#type").value;
+    if (checkEmail(email,type) && checkPassword(password)) {
+      let obj = {
+        username: email,
+        full_name: document.querySelector("#full_name").value,
+        password: password,
+        confirm_password: document.querySelector("#confirm_password").value,
+        question: document
+          .querySelector("#Safety_question")
+          .value.toLowerCase(),
+        type: type,
+        about_you: "",
+        location: "",
+        education: "",
+        jobTitle: "",
+        lookingForWork: "",
+      };
+      if (obj.password != obj.confirm_password) {
+        id_warning.innerHTML += "Passwords do not match!";
+        return;
+      }
+      let flag = 0;
+      //arr_students = Array.from(arr_students)
+      arr_students.forEach((element) => {
+        if (element["username"] == obj.username) {
+          flag = 1;
+        }
+      });
+      arr_inspector.forEach((element) => {
+        if (element["username"] == obj.username) {
+          flag = 1;
+        }
+      });
+
+      arr_hr.forEach((element) => {
+        if (element["username"] == obj.username) {
+          flag = 1;
+        }
+      });
+      if (flag == 1) {
+        id_warning.innerHTML = "Account already exists!";
+        return;
+      }
+
+      if (obj.type == "student_graduate") {
+        arr_students.push(obj);
+    }
+  };
+}
+describe('register',()=>{
+  it("Should add the new user to storage",()=>{
+    //at first the students storage is empty
+    expect(arr_students).toEqual([])
+    //then we activate the function
+    register();
+    //now there's a user in it
+    expect(arr_students).not.toEqual([])
+    expect(arr_students[0]).toEqual(user)
+  })
 
 it('Shouldnt be able to make 2 accounts',()=>{
-  let flag = 0;
-  arr_students.forEach((element) => {
-    if (element["username"] == user.username) {
-      flag = 1;
-    }
-  });
-  arr_inspector.forEach((element) => {
-    if (element["username"] == user.username) {
-      flag = 1;
-    }
-  });
-
-  arr_hr.forEach((element) => {
-    if (element["username"] == user.username) {
-      flag = 1;
-    }
-  });
-  if (flag == 1) {
-    id_warning.innerHTML += `${'Account already exists!'}`;
-  }
+  //at first we have empty storage
+  expect(arr_students).toEqual([]);
+  //then we activate the function
+  register();
+  expect(arr_students).not.toEqual([])
+  expect(arr_students[0]).toEqual(user);
+  //then we activate the function again
+  register();
   expect(id_warning.innerHTML).toBe('Account already exists!')
 })
-  
+it('Passwords should match',()=>{
+  //set up the confirm_pass element to be different than pass
+  confirm_pass.value = 'wrongpass'
+  //call the function
+  register();
+  expect(id_warning.innerHTML).toBe('Passwords do not match!')
+})
+});
 describe('checkEmail', () => {
   it('should check that an email address is valid(contains "@ac.sce.ac.il") and that the user type is student',()=>{
     let email = 'student1@ac.sce.ac.il';
     let type = 'student_graduate'
-    expect(func.checkEmail(email,type)).toBe(true);
+    expect(checkEmail(email,type)).toBe(true);
     let email2 = 'student2@gmail.com';
     let type2 = 'student_graduate'
-    expect(func.checkEmail(email2,type2)).toBe(false);
+    expect(checkEmail(email2,type2)).toBe(false);
     let email3 = 'hr@ac.sce.ac.il';
     let type3 = 'hr'
-    expect(func.checkEmail(email3,type3)).toBe(false);
+    expect(checkEmail(email3,type3)).toBe(false);
     let email4 = 'inspector@ac.sce.ac.il';
     let type4 = 'inspector'
-    expect(func.checkEmail(email4,type4)).toBe(false);
+    expect(checkEmail(email4,type4)).toBe(false);
   })
 })
 
@@ -91,7 +162,7 @@ describe('checkPassword', () => {
         const password = 'ValidPassw0rd!';
 
         // Call the function
-        const result = func.checkPassword(password);
+        const result = checkPassword(password);
 
         // Assert that the function returned true
         expect(result).toBe(true);
@@ -102,7 +173,7 @@ describe('checkPassword', () => {
         const password = 'invalidpassworD1';
 
         // Call the function
-        const result = func.checkPassword(password);
+        const result = checkPassword(password);
 
         // Assert that the function returned false
         expect(result).toBe(false);
@@ -112,7 +183,7 @@ describe('checkPassword', () => {
         const password = 'invalidpassword2!';
 
         // Call the function
-        const result = func.checkPassword(password);
+        const result = checkPassword(password);
 
         // Assert that the function returned false
         expect(result).toBe(false);
@@ -122,7 +193,7 @@ describe('checkPassword', () => {
         const password = 'INVALIDPASSW0RD!';
 
         // Call the function
-        const result = func.checkPassword(password);
+        const result = checkPassword(password);
 
         // Assert that the function returned false
         expect(result).toBe(false);
@@ -132,7 +203,7 @@ describe('checkPassword', () => {
         const password = 'Inval1!';
 
         // Call the function
-        const result = func.checkPassword(password);
+        const result = checkPassword(password);
 
         // Assert that the function returned false
         expect(result).toBe(false);
@@ -142,27 +213,9 @@ describe('checkPassword', () => {
         const password = 'invalidpassworD!';
 
         // Call the function
-        const result = func.checkPassword(password);
+        const result = checkPassword(password);
 
         // Assert that the function returned false
         expect(result).toBe(false);
     });
 });
-
-  describe('declareEvents', () => {
-    it('Check that the register button is working', () => {
-      // Set up a mock add button element
-      const addButton = document.createElement('button');
-      addButton.id = 'signupbtn';
-      document.body.appendChild(addButton);
-  
-      // Spy on the addEventListener method
-      jest.spyOn(addButton, 'addEventListener');
-  
-      // Call the declareEvents function
-      func.declareEvents();
-  
-      // Assert that the addEventListener method was called with the correct arguments
-      expect(addButton.addEventListener).toHaveBeenCalledWith('click', expect.any(Function));
-    });
-  });
