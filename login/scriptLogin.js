c30083b4fe2bde2aa9352324466f284f2ee234f7
Yonatan <yonatanguetta9696@gmail.com>
2023-01-14 14:21:02 +0200

    lastone

--- login/scriptLogin.js
+++ login/scriptLogin.js
@@ -2,60 +2,60 @@ let arr_students =
   JSON.parse(localStorage.getItem("Students and Graduates")) || [];
 let arr_hr = JSON.parse(localStorage.getItem("HR")) || [];
 let arr_inspector = JSON.parse(localStorage.getItem("Inspectors")) || [];
-let warning = '';
+let warning = "";
 console.log(module.exports);
 
-function checkPassword(password){
+function checkPassword(password) {
   //Password must be 8 to 15 characters which contain at least one lowercase letter,
   //one uppercase letter, one numeric digit, and one special character
   var passw = /^(?=.\d)(?=.[a-z])(?=.[A-Z])(?=.[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
-  
-  if(password.match(passw)){
+
+  if (password.match(passw)) {
     return true;
   }
-  warning = "The password is too weak!"
+  warning = "The password is too weak!";
   return false;
-};
+}
 
-function checkInput(question,email,newpassword){
+function checkInput(question, email, newpassword) {
   //this function iterates through all users until it finds one that matches email
   arr_students.forEach((element) => {
-    if (element["username"] == email) {//emails are unique so if the email is equal just check for question
-      if(element["question"] == question){//if question is equal update the new password
-        element["password"] = newpassword;//update password
+    if (element["username"] == email) {
+      //emails are unique so if the email is equal just check for question
+      if (element["question"] == question) {
+        //if question is equal update the new password
+        element["password"] = newpassword; //update password
         element["confirm_password"] = newpassword;
-        localStorage.setItem( "Students and Graduates",JSON.stringify(element));//update local storage
-        window.location.href = "../login/login_n.html";//move to login page
+        localStorage.setItem("Students and Graduates", JSON.stringify(element)); //update local storage
+        window.location.href = "../login/login_n.html"; //move to login page
       }
     }
   });
   arr_inspector.forEach((element) => {
     if (element["username"] == email) {
-      if(element["question"] == question){
+      if (element["question"] == question) {
         element["password"] = newpassword;
         element["confirm_password"] = newpassword;
-        localStorage.setItem( "Inspectors",JSON.stringify(element));  
+        localStorage.setItem("Inspectors", JSON.stringify(element));
         window.location.href = "../login/login_n.html";
-      }
-      else{
+      } else {
         return;
-       }
+      }
     }
   });
 
   arr_hr.forEach((element) => {
     if (element["emailC"] == email) {
-      if(element["question"] == question){
-      element["password"] = newpassword;
-      localStorage.setItem( "HR",JSON.stringify(element));  
-      window.location.href = "../login/login_n.html";
-      }
-      else{
+      if (element["question"] == question) {
+        element["password"] = newpassword;
+        localStorage.setItem("HR", JSON.stringify(element));
+        window.location.href = "../login/login_n.html";
+      } else {
         return;
       }
     }
   });
-};
+}
 
 function signinaction(event) {
   event.preventDefault();
@@ -91,33 +91,35 @@ function signinaction(event) {
     id_warning.innerHTML += `${warning}`;
     //return;
   }
-};
+}
 
 function changepassword(event) {
   event.preventDefault();
-  const email = document.getElementById('emailUser').value.toLowerCase();
-  const question = document.getElementById('safetyQuestion').value.toLowerCase();
-  const newpassword = document.getElementById('newPassword').value;
-  const confirmpassword = document.getElementById('confirmPassword').value;
-  if (email == "" || question == "" || confirmpassword == "" || newpassword == "") {
-    warning ="Please fill all the details";
+  const email = document.getElementById("emailUser").value.toLowerCase();
+  const question = document
+    .getElementById("safetyQuestion")
+    .value.toLowerCase();
+  const newpassword = document.getElementById("newPassword").value;
+  const confirmpassword = document.getElementById("confirmPassword").value;
+  if (
+    email == "" ||
+    question == "" ||
+    confirmpassword == "" ||
+    newpassword == ""
+  ) {
+    warning = "Please fill all the details";
+  } else if (newpassword != confirmpassword) {
+    warning = "Passwords mismatch";
+  } else if (!checkPassword(newpassword)) {
+  } else {
+    checkInput(question, email, newpassword);
   }
-  else if (newpassword != confirmpassword) {
-    warning ="Passwords mismatch";
-  }
-  else if(!checkPassword(newpassword)){
-    
-  }
-  else{
-    checkInput(question,email,newpassword);
-  }
-  document.getElementById('texto').innerHTML = warning;
+  document.getElementById("texto").innerHTML = warning;
+}
+
+module.exports = {
+  signinaction: signinaction,
+  changepassword: changepassword,
+  checkPassword: checkPassword,
+  checkInput: checkInput,
 };
-
-  module.exports = {
-
-    signinaction: signinaction,
-    changepassword: changepassword,
-    checkPassword: checkPassword,
-    checkInput: checkInput
-  };
